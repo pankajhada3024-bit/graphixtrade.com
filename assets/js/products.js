94502998642674c6b06f497a03ed358141ec5406
pankajhada3024-bit <pankajhada3024@gmail.com>
2026-01-17 19:25:07 +0530

    Update products.js

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,1 +1,23 @@
+document.querySelectorAll(".product-slider").forEach(slider => {
+  const images = slider.querySelectorAll("img");
+  const prev = slider.querySelector(".prev");
+  const next = slider.querySelector(".next");
+
+  let index = 0;
+
+  function showImage(i) {
+    images.forEach(img => img.classList.remove("active"));
+    images[i].classList.add("active");
+  }
+
+  prev.addEventListener("click", () => {
+    index = (index - 1 + images.length) % images.length;
+    showImage(index);
+  });
+
+  next.addEventListener("click", () => {
+    index = (index + 1) % images.length;
+    showImage(index);
+  });
+});
 
